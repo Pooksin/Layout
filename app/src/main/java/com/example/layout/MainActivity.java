7944da24a8f5907728af6c4fbda6d4dbb67a793e
krsl3499 <cxsa418@gmail.com>
2021-03-12 02:11:16 +0900

    레이아웃 초안

---
+++ app/src/main/java/com/example/layout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void popup(View v){
+        Intent intent = new Intent(MainActivity.this,SubAct.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/layout/SubAct.java
@@ -0,0 +1,29 @@
+package com.example.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class SubAct extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_sub);
+
+        Button closeBtn = findViewById(R.id.closeBtn);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+}
